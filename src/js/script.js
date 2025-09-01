ce4d50d21ddd6fb07800a082936355971283562d
depstor <depstor90@gmail.com>
2025-09-01 15:41:30 +0300

    gsap

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import { gsap } from 'gsap';
+
 import '/src/sass/style.scss';
+
+gsap.to(review, {
+    duration: 2,
+    x: 300,
+    opacity: 0.5,
+    ease: 'power2.inOut',
+    repeat: -1,
+    yoyo: true
+})
